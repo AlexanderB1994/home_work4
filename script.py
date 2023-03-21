6d9bf9f1c6d5d89c505cef58e86a9ca0cc80aafa
AlexanderB1994 <bulgaru.alexander1994@gmail.com>
2023-03-21 15:31:03 +0200

    home_work4 update

--- script.py
+++ script.py
@@ -17,16 +17,12 @@ print(get_sum_from_parameters(2, 4, "abc", param_1=2, param_2="abc", param_3=2.5
 # 2
 def get_sum_from_parameters(*args, **kwargs):
     total = 0
-    for arg in args:
-        if isinstance(arg, (int, float)):
-            total += arg
-        elif isinstance(arg, (list, tuple, set)):
-            total += get_sum_from_parameters(*arg)
-    for value in kwargs.values():
-        if isinstance(value, (int, float)):
-            total += value
-        elif isinstance(value, (list, tuple, set)):
-            total += get_sum_from_parameters(*value)
+    parameters = list(args) + list(kwargs.values())
+    for parameter in parameters:
+        if isinstance(parameter, (int, float)):
+            total += parameter
+        elif isinstance(parameter, (list, tuple, set)):
+            total += get_sum_from_parameters(*parameter)
     return total
 
 
